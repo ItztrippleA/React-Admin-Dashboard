a056a242f37dcb49984514ca74e615bb088b905b
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-09-10 06:31:54 +0100

    Final commit for the multpurpose admin dashboard with react would serve as boilerplate for future need and implementation of an admin backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Topbar from "./components/topBar/Topbar";
 import "./app.css";
 import Home from "./pages/Home/Home";
 import UserList from "./pages/UserList/UserList";
+import User from "./pages/User/User";
+import NewUser from "./pages/NewUser/NewUser";
+import ProductList from "./pages/ProductList/ProductList";
+import Product from "./pages/Product/Product";
+import NewProduct from "./pages/NewProduct/NewProduct";
 
 function App() {
   return (
@@ -17,6 +22,21 @@ function App() {
           </Route>
           <Route path="/users">
             <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newProduct">
+            <NewProduct />
           </Route>
         </Switch>
       </div>
